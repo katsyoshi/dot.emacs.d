bb3498ef06aa25c4bbff06d3230a9659c575880a
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2015-03-28 21:57:30 +0900

    cosmetic :lipstick:

--- settings/yaml.el
+++ settings/yaml.el
@@ -2,5 +2,7 @@
 (setq auto-mode-alist
       (cons (cons "\\.yml$" 'yaml-mode) auto-mode-alist))
 (setq indent-tabs-mode nil)
-(add-hook 'yaml-mode-hook '(lambda () (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
+(add-hook 'yaml-mode-hook
+          '(lambda ()
+             (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
 
