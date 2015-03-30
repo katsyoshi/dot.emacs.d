57018ec5f5dc4388acd48870d3d92cd8d4b2ce35
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2015-03-30 15:35:10 +0900

    cosmetic :lipstick:

--- settings/git.el
+++ settings/git.el
@@ -1,13 +1,16 @@
 (require 'magit)
+(require 'magit-gh-pulls)
+
 (require 'git-rebase-mode)
 (require 'git-commit-mode)
 (require 'gitconfig-mode)
 (require 'gitignore-mode)
 (require 'gitattributes-mode)
+
 (global-set-key (kbd "M-g M-b") 'magit-branch-manager)
 (global-set-key (kbd "M-g M-c") 'magit-checkout)
 (global-set-key (kbd "M-g M-g") 'magit-status)
 (global-set-key (kbd "M-g M-u") 'magit-pull)
 (global-set-key (kbd "M-g M-s") 'vc-git-grep)
-(require 'magit-gh-pulls)
+
 (add-hook 'magit-mode-hook 'turn-on-magit-gh-pulls)
