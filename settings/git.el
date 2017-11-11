9c29eaa989dd5c7dc58aa228a4fe539849a98684
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2017-11-11 14:43:40 +0900

    change magit-status command

--- settings/git.el
+++ settings/git.el
@@ -1,8 +1,4 @@
 (require 'magit)
 (setq magit-last-seen-setup-instructions "1.4.0")
 
-(global-set-key (kbd "M-g M-b") 'magit-branch-manager)
-(global-set-key (kbd "M-g M-c") 'magit-checkout)
-(global-set-key (kbd "M-g M-g") 'magit-status)
-(global-set-key (kbd "M-g M-u") 'magit-pull)
-(global-set-key (kbd "M-g M-s") 'vc-git-grep)
+(global-set-key (kbd "C-c C-s") 'magit-status)
