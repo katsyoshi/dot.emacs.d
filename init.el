d4741ba9c184e857dd40f4d8722fe1b0eaa16e87
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2018-06-19 10:03:02 +0900

    remove package.el initial settings

--- init.el
+++ init.el
@@ -1,12 +1,5 @@
 ;; .emacs.d/init.el
 ;; set font and background color
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
-
 (set-background-color "#98bc98")
 (set-foreground-color "black")
 ;; バックスペース
