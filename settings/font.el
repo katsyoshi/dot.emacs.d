56f187a278581fcfa01d27dd4706d346bacd6af3
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2019-03-07 11:09:23 +0900

    Recity is not aligned spaces in the file of using Japanese.

--- settings/font.el
+++ settings/font.el
@@ -1,4 +1,4 @@
-(set-face-attribute 'default nil :family "Ricty" :height 120)
+(set-face-attribute 'default nil :family "Noto Sans Mono" :height 110)
 
 (global-set-key (kbd "M-C-<up>") 'text-scale-increase)
 (global-set-key (kbd "M-C-<down>") 'text-scale-decrease)
