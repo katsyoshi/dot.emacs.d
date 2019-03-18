b92534b078ae7ce7c691a8770d94ea1b032c2918
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2019-03-18 22:51:06 +0900

    javascript files use js2-mode

--- settings/lsp/javascript.el
+++ settings/lsp/javascript.el
@@ -1,7 +1,8 @@
 (require 'js2-mode)
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (add-hook 'js2-mode-hook
           (lambda ()
-             (setq js2-basic-offset 2)
-             (setq js-switch-indent-offset 2)))
+            (setq js2-basic-offset 2)
+            (setq js-switch-indent-offset 2)))
 (add-hook 'js2-mode-hook #'lsp)
 (add-hook 'js2-mode-hook #'flycheck)
