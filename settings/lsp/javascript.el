fdc77ada3600c12b3148d483e9394f44432f0f7e
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2019-03-19 10:48:43 +0900

    add-hook for js2-mode

--- settings/lsp/javascript.el
+++ settings/lsp/javascript.el
@@ -3,6 +3,6 @@
 (add-hook 'js2-mode-hook
           (lambda ()
             (setq js2-basic-offset 2)
-            (setq js-switch-indent-offset 2)))
-(add-hook 'js2-mode-hook #'lsp)
-(add-hook 'js2-mode-hook #'flycheck)
+            (setq js-switch-indent-offset 2)
+            (lsp)
+            (flycheck)))
