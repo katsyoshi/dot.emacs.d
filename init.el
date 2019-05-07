f8260224127cd913f7b60bb55525b0c891c32238
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2019-05-07 10:49:30 +0900

    add sass-mode

--- init.el
+++ init.el
@@ -99,6 +99,7 @@
 (load "~/.emacs.d/settings/git.el")
 (load "~/.emacs.d/settings/lsp.el")
 (load "~/.emacs.d/settings/markdown.el")
+(load "~/.emacs.d/settings/sass.el")
 (load "~/.emacs.d/settings/skk.el")
 (load "~/.emacs.d/settings/yaml.el")
 
--- settings/sass.el
+++ settings/sass.el
@@ -1,2 +1,4 @@
+(add-to-list 'auto-mode-alist '("\\.scss$" . scss-mode))
+(add-to-list 'auto-mode-alist '("\\.sass$" . scss-mode))
 (setq css-indent-level 2)
 (setq css-indent-offset 2)
