4fd0ab87311330eb3f351e8357af18252e3fc642
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2019-08-09 11:53:15 +0900

    supprot json

--- settings/lsp/javascript.el
+++ settings/lsp/javascript.el
@@ -1,5 +1,6 @@
 (require 'js2-mode)
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.json\\'" . js2-mode))
 (add-hook 'js2-mode-hook
           (lambda ()
             (setq js2-basic-offset 2)
