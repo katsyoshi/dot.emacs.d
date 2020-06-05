a53bca7521b1e435bdbc8d527484397795172863
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2020-06-05 22:39:30 +0900

    change key-bind for reset fontsize

--- settings/font.el
+++ settings/font.el
@@ -2,4 +2,4 @@
 
 (global-set-key (kbd "M-C-<up>") 'text-scale-increase)
 (global-set-key (kbd "M-C-<down>") 'text-scale-decrease)
-(global-set-key (kbd "M-C-a") 'text-scale-adjust)
+(global-set-key (kbd "M-C-0") 'text-scale-adjust)
