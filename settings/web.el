f47e588b56b270890fb684eb9f30f5dda97dfc94
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2023-02-02 16:10:35 +0900

    add web-mode

---
+++ settings/web.el
@@ -0,0 +1,6 @@
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.ts[x]?\\'" . web-mode))
+
+(setq web-mode-markup-indent-offset 2)
+(setq web-mode-css-indent-offset 2)
+(setq web-mode-code-indent-offset 2)
