55d4c99bb26e5ca01834da6bd58007030e6b43b5
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2024-06-07 11:17:22 +0900

    remove doom-theme

--- settings/doom.el
+++ settings/doom.el
@@ -5,10 +5,6 @@
 (setq doom-themes-enable-italic t)
 (setq doom-themes-enable-bold t)
 
-(load-theme 'doom-dracula t)
-(doom-themes-neotree-config)
-(doom-themes-org-config)
-
 (custom-set-faces
  '(doom-modeline-bar ((t (:background "#6272a4")))))
 
@@ -20,8 +16,3 @@
 (setq column-number-mode 0)
 
 (add-hook 'after-init-hook #'doom-modeline-mode)
-
-(doom-modeline-def-modeline
-  'main
-  '(bar window-number matches buffer-info remote-host buffer-position parrot selection-info)
-  '(misc-info persp-name lsp github debug minor-modes input-method major-mode process vcs checker))
